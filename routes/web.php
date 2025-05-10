bc22a082627d748a8b8bd1f80188c978fcf3343c
Arjunstein <arjun.gnw09@gmail.com>
2025-05-11 00:31:27 +0700

    ✨ feat: crud category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Livewire\Category\CreateCategory;
+use App\Livewire\Category\EditCategory;
+use App\Livewire\Category\ListCategory;
 use App\Livewire\Dashboard\Dashboard;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +11,6 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', Dashboard::class)->name('dashboard');
+Route::get('/category', ListCategory::class)->name('category');
+Route::get('/category/create', CreateCategory::class)->name('category-create');
+Route::get('/category/edit/{id}', EditCategory::class)->name('category-edit');
